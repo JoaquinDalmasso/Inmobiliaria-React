73d95e413af650fcb72e5838599e9117b49e864a
joacodalmasso <joacodalmasso@gmail.com>
2024-07-24 22:37:41 +0000

    Redireccionado a Home

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -3,9 +3,10 @@ import {app} from '../firebase'
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-
+    const navigate = useNavigate();
     const dispach = useDispatch();
     const handleGoogleClick = async () => {
     try {
@@ -26,7 +27,8 @@ export default function OAuth() {
             });
 
             const data = await res.json();
-            dispach(signInSucces(data));
+            dispach(signInSuccess(data));
+            navigate('/');
     } catch (error) {
         console.log('could not sign in with google', error)
     }
